d62a8882c80d5472a9569e60982ab393d4fdec45
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-04-23 10:27:57 +1000

    mfixed d test

--- app/spec/spec_leaderboard.rb
+++ app/spec/spec_leaderboard.rb
@@ -4,19 +4,17 @@ require_relative '../leaderboard'
 RSpec.describe Leaderboard do
   let(:player1) { Player.new(name: 'Roboman', sign: 'X', scores: 2) }
   let(:player2) { Player.new(name: 'Isabela', sign: 'O', scores: 4) }
-  let(:leaderboard) {Leaderboard.new(players: [player2, player1])}
+  let(:leaderboard) { Leaderboard.new(players: [player2, player1])}
 
 
-  describe '#top_scored_player' do
+  describe '#top_scored_players' do
     it 'returns desending order sorted array of players' do
-
-      expect(leaderboard.top_scored_player).to eq([player2, player1])
+      expect(leaderboard.top_scored_players).to eq([player2, player1])
     end
   end
 
   describe '#winners_name' do
     it 'returns the name of winner' do
-
       expect(leaderboard.winners_name).to eq('Isabela')
     end
   end
@@ -24,27 +22,17 @@ RSpec.describe Leaderboard do
   describe '#has_winner?' do
     context 'when there is a winner' do
       it 'returns true' do
-
         expect(leaderboard.has_winner?).to eq(true)
       end
     end
 
     context 'when there is no winner / it is a draw' do
+      let(:player1) { Player.new(name: 'Roboman', sign: 'X', scores: 2) }
+      let(:player2) { Player.new(name: 'Isabela', sign: 'O', scores: 2) }
+      let(:leaderboard) {Leaderboard.new(players: [player2, player1])}
       it 'returns false' do
-        let(:player1) { Player.new(name: 'Roboman', sign: 'X', scores: 2) }
-        let(:player2) { Player.new(name: 'Isabela', sign: 'O', scores: 2) }
-        let(:leaderboard) {Leaderboard.new(players: [player2, player1])}
-
         expect(leaderboard.has_winner?).to eq(false)
       end
     end
   end
-
-
-
-
-
-
-
-
 end
