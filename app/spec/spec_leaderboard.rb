5445338f4ae9000b59424ab5a7c3b7f5fcfd07c9
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-04-23 10:11:34 +1000

    add leaderboard testing

---
+++ app/spec/spec_leaderboard.rb
@@ -0,0 +1,50 @@
+require_relative '../player'
+require_relative '../leaderboard'
+
+RSpec.describe Leaderboard do
+  let(:player1) { Player.new(name: 'Roboman', sign: 'X', scores: 2) }
+  let(:player2) { Player.new(name: 'Isabela', sign: 'O', scores: 4) }
+  let(:leaderboard) {Leaderboard.new(players: [player2, player1])}
+
+
+  describe '#top_scored_player' do
+    it 'returns desending order sorted array of players' do
+
+      expect(leaderboard.top_scored_player).to eq([player2, player1])
+    end
+  end
+
+  describe '#winners_name' do
+    it 'returns the name of winner' do
+
+      expect(leaderboard.winners_name).to eq('Isabela')
+    end
+  end
+
+  describe '#has_winner?' do
+    context 'when there is a winner' do
+      it 'returns true' do
+
+        expect(leaderboard.has_winner?).to eq(true)
+      end
+    end
+
+    context 'when there is no winner / it is a draw' do
+      it 'returns false' do
+        let(:player1) { Player.new(name: 'Roboman', sign: 'X', scores: 2) }
+        let(:player2) { Player.new(name: 'Isabela', sign: 'O', scores: 2) }
+        let(:leaderboard) {Leaderboard.new(players: [player2, player1])}
+
+        expect(leaderboard.has_winner?).to eq(false)
+      end
+    end
+  end
+
+
+
+
+
+
+
+
+end
