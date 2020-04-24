82e729e9063fecf2f0f1247b4b232e6e1c5af74d
Syeda Ismat Farjana <diptyfmc17@gmail.com>
2020-04-24 23:53:12 +1000

    creat one round

--- app/tic_tac_toe.rb
+++ app/tic_tac_toe.rb
@@ -1,7 +1,10 @@
 require 'lolcat'
+require 'colorize'
 require_relative 'game'
 require_relative 'board'
 require_relative 'player'
+require_relative 'leaderboard'
+
 
 puts '
 ░██╗░░░░░░░██╗███████╗██╗░░░░░░█████╗░░█████╗░███╗░░░███╗███████╗
@@ -45,23 +48,42 @@ sleep 1
 
 game = Game.new(rounds: rounds)
 board = Board.new
+leaderboard = Leaderboard.new(players: [player, computer])
+
+def take_position(player: , computer: , board:, game:)
+  if board.continue_round?(player: player, computer: computer)
+    puts 'Cʜᴏᴏsᴇ ᴀ ᴘʟᴀᴄᴇ ғʀᴏᴍ 1 ᴛᴏ 9 ᴛᴏ ᴘʟᴀᴄᴇ ʏᴏᴜʀ sɪɢɴ ᴏɴ ᴛʜᴇ ʙᴏᴀʀᴅ'
+    position = gets.chomp.to_i
+    board.place_sign(sign: player.sign, position: position)
+    sleep 1
+    computers_position = board.available_places.sample
+    board.place_sign(sign: computer.sign, position: computers_position)
+    puts "Bᴏᴛ ᴄʜᴏᴏsᴇ ᴘᴏsɪᴛɪᴏɴ #{computers_position}"
+    sleep 1
+    board.display
+    take_position(player: player, computer: computer, board: board, game: game)
+  else
+    game.reduce_round
+    if board.winning_pattern?(sign: player.sign)
+      player.increase_scores
+      puts "Yᴏᴜ ᴡɪɴ ᴛʜɪs ʀᴏᴜɴᴅ"
+    elsif board.winning_pattern?(sign: computer.sign)
+      computer.increase_scores
+      puts "Bᴏᴛ ᴡɪɴs ᴛʜɪs ʀᴏᴜɴᴅ"
+    else
+      puts 'Tʜɪs ʀᴏᴜɴᴅ ᴡᴀs ᴀ ᴅʀᴀᴡ'
+    end
+  end
+end
 
 if game.round_left?
   board.display
-  puts 'Cʜᴏᴏsᴇ ᴀ ᴘʟᴀᴄᴇ ғʀᴏᴍ 1 ᴛᴏ 9 ᴛᴏ ᴘʟᴀᴄᴇ ʏᴏᴜʀ sɪɢɴ ᴏɴ ᴛʜᴇ ʙᴏᴀʀᴅ'
-  position = gets.chomp.to_i
-  board.place_sign(sign: player.sign, position: position)
-  sleep 1
-  computers_position = board.available_places.sample
-  board.place_sign(sign: computer.sign, position: computers_position)
-  puts "Bᴏᴛ ᴄʜᴏᴏsᴇ ᴘᴏsɪᴛɪᴏɴ #{computers_position}"
-  sleep 1
-  board.display
-else
-  puts ' '
-  puts ' '
-  puts '
-  █▀▀ ▄▀█ █▀▄▀█ █▀▀   █▀█ █░█ █▀▀ █▀█
-  █▄█ █▀█ █░▀░█ ██▄   █▄█ ▀▄▀ ██▄ █▀▄'
+  take_position(player: player, computer: computer, board: board, game: game)
+
 end
 
+puts ' '
+puts ' '
+puts '
+  █▀▀ ▄▀█ █▀▄▀█ █▀▀   █▀█ █░█ █▀▀ █▀█
+  █▄█ █▀█ █░▀░█ ██▄   █▄█ ▀▄▀ ██▄ █▀▄'
